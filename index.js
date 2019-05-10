deb46327b270706cf9d9d578836a712e43c97a1e
LarrySimiyu <larry.simiyu@gmail.com>
2019-05-10 09:44:48 -0700

    Stretch goal reached

--- index.js
+++ index.js
@@ -28,6 +28,66 @@ server.post('/api/actions', (req, res) => {
             res.status(500).json({ error: 'Failed to create action' })
         })
 })
+
+//STRETCH get
+server.get('/api/actions', (req, res) => {
+    db('actions')
+        .then(actions => {
+            res.status(200).json(actions);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        })
+})
+
+//STRETCH put 
+server.put('/api/actions/:id', (req, res) => {
+    db('actions')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if(count > 0) {
+                res.status(200).json(count)
+
+            } else {
+                res.status(404).json({ message: 'Action not found' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+
+// STRETCH delete
+server.delete('/api/actions/:id', (req, res) => {
+    db('actions')
+        .where({ di: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+            } else {
+                res.status(404).json({ message: 'action not found' });
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+//STRETCH POST 
+server.post('/api/projects', (req, res) => {
+    db('projects')
+        .insert(req.body)
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch( error => {
+            res.status(500).json(error)
+        });
+});
+
 //GET for retrieving a project by its ID
 server.get('/api/projects/:id', (req, res) => {
     const { id } = req.params;
@@ -49,6 +109,43 @@ server.get('/api/projects/:id', (req, res) => {
         })
 
 })
+
+//STRETCH PUT
+server.put('/api/projects/:id', (req, res) => {
+    db('projects')
+        .where({ id: req.params.id })
+        .update(req.body)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json(count)
+
+            } else {
+                res.status(404).json({ message: ' Project not found' })
+            }
+        })
+        .catch(error => {
+            res.status(500).json(error)
+        });
+});
+
+// STRETCH POST 
+
+//STRETCH DELETE
+server.delete('/api/projects/:id', (req, res) => {
+    db('projects')
+        .where({ id: req.params.id })
+        .del()
+        .then(count => {
+            if (count > 0) {
+                res.status(200).end();
+            } else {
+                res.status(404).json({ message: 'Project not found'});
+            }
+        })
+        .catch( error => {
+            res.status(500).json(error);
+        });
+});
 
 const PORT = 3000;
 server.listen(PORT, () => {
